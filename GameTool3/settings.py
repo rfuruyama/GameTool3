9f72cc157676aa986c6a7178cc3940f8fb6b9b41
suraimu601@icloud.com <suraimu601@icloud.com>
2023-03-16 20:48:31 +0900

    update settings

--- GameTool3/settings.py
+++ GameTool3/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,13 +21,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7c_hmpx^#q3=u=b3elg8)jj$1a*#nf9p2%%zm33nhrdx8zs1ha'
 
+SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com', 'rfuruyama.pythonanywhere.com']
 
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
@@ -120,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 AUTH_USER_MODEL = 'AKTool.User'
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except:
+    pass
